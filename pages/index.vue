0330977f05524a5f10f1d7beae139e1e23d71672
Lucho2027 <luis01.ro@gmail.com>
2024-08-08 07:21:28 -0500

    Installing daisyui

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+<div class="flex flex-col min-h-dvh">
+  <header class="px-4 lg:px-6 h-14 flex items-center">
+    <a class="flex items-center justify-center pt-2" href="#">
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+            <path fill="#ffffff" d="M12 4.942c1.827 1.105 3.474 1.6 5 1.833v7.76c0 1.606-.415 1.935-5 4.76v-14.353zm9-1.942v11.535c0 4.603-3.203 5.804-9 9.465-5.797-3.661-9-4.862-9-9.465v-11.535c3.516 0 5.629-.134 9-3 3.371 2.866 5.484 3 9 3zm-2 1.96c-2.446-.124-4.5-.611-7-2.416-2.5 1.805-4.554 2.292-7 2.416v9.575c0 3.042 1.69 3.83 7 7.107 5.313-3.281 7-4.065 7-7.107v-9.575z"/></svg>
+      <span class="sr-only">Guild's Squire</span>
+    </a>
+    <nav class="ml-auto flex gap-4 sm:gap-6">
+    </nav>
+  </header>
+  <main class="flex-1">
+    <section class="w-full pt-12 md:pt-24 lg:pt-32">
+      <div class="container px-4 md:px-6 space-y-10 xl:space-y-16">
+        <div class="grid gap-4 lg:grid-cols-[1fr_550px] lg:gap-16">
+          <div class="flex flex-col justify-center space-y-4">
+            <div class="space-y-2">
+              <h1 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+                Let's get your Guild's needs taken care in one place
+              </h1>
+              <p class="max-w-[700px] text-muted-foreground md:text-xl">
+                Streamline your World Of Warcraft Guild's events, manage rules keep track of loot and attendance all leveraging Battle.net API for World Of Warcraft
+              </p>
+            </div>
+            <div class="flex flex-col gap-2 min-[400px]:flex-row">
+              <a
+                href="#"
+                class="btn btn-lg btn-accent">
+                Let's get you logged In
+              </a>
+              <a
+                href="#"
+                class="btn btn-lg btn-primary">
+                Let's get you logged In
+              </a>   
+            </div>
+          </div>
+          <img
+            src=""
+            width="550"
+            height="550"
+            alt="Product"
+            class="mx-auto aspect-square overflow-hidden rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer class="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+    <p class="text-xs text-muted-foreground">© Guild's Squire All rights reserved.</p>
+  </footer>
+</div>
+  </div>
+</template>
